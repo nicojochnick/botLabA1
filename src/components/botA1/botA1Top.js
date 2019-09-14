ca369f1c7c4412370164c1e8e6dd92ed6c0d6484
Nico Jochnick <nico.jochnick@berkeley.edu>
2019-09-14 06:56:40 -0700

    to origin

--- src/components/botA1/botA1Top.js
+++ src/components/botA1/botA1Top.js
@@ -23,10 +23,7 @@ class BotA1Top extends Component {
             currentStep: this.getStep(),
             steps: null
         }
-
     };
-
-
 
     handleEnd({ steps, values }) {
         this.setState({currentStep: StepsBot.test});
